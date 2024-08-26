cf16eea0e387c3a0e2933d3681f2dd55cd657943
Mazin Islam <zamzam2960@gmail.com>
2024-08-26 15:52:47 +0300

    feat: Add send messages page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,119 @@
-import Image from "next/image";
-
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [file, setFile] = useState<File | null>(null);
+	const [message, setMessage] = useState('');
+	const [dateTime, setDateTime] = useState('');
+	const router = useRouter();
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		const expiryTime = localStorage.getItem('tokenExpiry');
+		if (!token || !expiryTime) {
+			alert('Please login first');
+			router.push('/login');
+			return;
+		}
+		const now = new Date();
+		if (parseInt(expiryTime) < now.getTime()) {
+			alert('Session expired, please login again');
+			localStorage.removeItem('token');
+			localStorage.removeItem('tokenExpiry');
+			router.push('/login');
+		}
+	}, [router]);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const getMinDateTime = () => {
+		const now = new Date();
+		now.setMinutes(now.getMinutes() + 1); // Add 1 minute
+		now.setSeconds(0, 0); // Set seconds and milliseconds to 0
+		return now.toISOString().slice(0, 16); // Format without seconds
+	};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		if (!file || !message || !dateTime) {
+			alert('Please fill in all fields');
+			return;
+		}
+		// if the date is in the past or is right now then alert the user
+		const now = new Date();
+		const selectedDate = new Date(dateTime);
+		if (selectedDate.getTime() <= now.getTime() || selectedDate.getTime() - now.getTime() < 60000) {
+			alert('Please select a date and time at least 1 minute in the future');
+			return;
+		}
+		const formData = new FormData();
+		formData.append('file', file);
+		formData.append('message', message);
+		formData.append('date', dateTime);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+		try {
+			const response = await fetch('http://localhost:3000/send-messages', {
+				method: 'POST',
+				body: formData,
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`,
+				},
+			});
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+			if (response.ok) {
+				alert('Message sent successfully');
+			} else {
+				if (response.status === 400) {
+					alert('Please fill in all fields');
+					return;
+				} else if (response.status === 401) {
+					alert('Please login first');
+				} else alert('Failed to send message');
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			alert('An error occurred while sending the message');
+		}
+	};
+
+	return (
+		<main
+			className='flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100'
+			dir='rtl'
+		>
+			<form
+				onSubmit={handleSubmit}
+				className='flex flex-col space-y-6 bg-white p-8 rounded-lg shadow-lg w-full max-w-md'
+			>
+				<h1 className='text-2xl font-bold text-center text-gray-800'>إرسال رسالة</h1>
+				<input
+					type='file'
+					accept='.xlsx, .xls'
+					onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
+					className='border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500'
+					required
+				/>
+				<textarea
+					placeholder='أدخل رسالتك'
+					rows={4}
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+					className='border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500'
+					required
+				/>
+				<input
+					type='datetime-local'
+					value={dateTime}
+					min={getMinDateTime()}
+					onChange={(e) => setDateTime(e.target.value)}
+					className='border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500'
+					required
+				/>
+				<button
+					type='submit'
+					className='bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300'
+				>
+					إرسال
+				</button>
+			</form>
+		</main>
+	);
 }
